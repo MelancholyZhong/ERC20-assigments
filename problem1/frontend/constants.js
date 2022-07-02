84a30ce7a8de09400df42546d19f4c43587dad1a
MelancholyZhong <devin.zy36@gmail.com>
2022-07-01 19:12:32 -0700

    tested on mumbai network

--- problem1/frontend/constants.js
+++ problem1/frontend/constants.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+export const contractAddress = "0xB350ed29D6890A967706D03883d6300018978Bd4"
 
 export const abi = [
     {
